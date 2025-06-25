6e34574f6761dfd1b07b5f2b2ad39289d53e97cb
AndyBS1 <andy230505@gmail.com>
2025-06-25 11:43:35 -0600

    Tabla de inventario

--- app/controllers/inventario_controller.rb
+++ app/controllers/inventario_controller.rb
@@ -1,5 +1,13 @@
 class InventarioController < ApplicationController
   def index
+    path = Rails.root.join("app", "data", "productos.json")
+
+    if File.exist?(path)
+      file = File.read(path)
+      @productos = JSON.parse(file, symbolize_names: true)
+    else
+      @productos = []
+    end
   end
 
   def agregar
